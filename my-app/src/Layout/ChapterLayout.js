d94d5ad32e5e4b0d66f1b8b6964552d8c4736451
Utchash007 <shariarhasan872@gmail.com>
2024-12-18 11:02:21 +0600

    ChapterLayout is updated and connected to API

--- my-app/src/Layout/ChapterLayout.js
+++ my-app/src/Layout/ChapterLayout.js
@@ -1,7 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CptIntro from '../template/CptIntro';
 import CptContent from '../template/CptContent';
+import { useParams } from 'react-router-dom';
+import axios from "axios";
 const ChapterLayout=()=>{
+
+    const id=useParams();
+    const [chapterdata, setChapterData] = useState(null);
+    const [noveldata, setNovelData] = useState(null);
+   useEffect(()=>{
+        const url1="http://127.0.0.1:8000/api/novel-info/chapters?novel_id="+id;
+        const url2="http://127.0.0.1:8000/api/novels/novel/?novel_id="+id;
+       Promise.all( 
+        [
+            axios.get(url1),
+            axios.get(url2)
+        ])
+        .then(([chapterDataResponse,novelDataResponse])=>{
+            setChapterData(chapterDataResponse.data);
+            setNovelData(novelDataResponse.data);
+        })
+        .catch((error)=>{
+            alert(error.message);
+          })
+
+   },[id]);
+
+
     return(
         <div>
             <div class="content-wrap">

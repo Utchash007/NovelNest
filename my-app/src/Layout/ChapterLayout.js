44bea4e91a44956bbf46edcf2fd60248acba7a6e
Utchash007 <shariarhasan872@gmail.com>
2024-12-18 17:22:22 +0600

    Dynamically routed

--- my-app/src/Layout/ChapterLayout.js
+++ my-app/src/Layout/ChapterLayout.js
@@ -5,12 +5,13 @@ import { useParams } from 'react-router-dom';
 import axios from "axios";
 const ChapterLayout=()=>{
 
-    const id=useParams();
-    const [chapterdata, setChapterData] = useState(null);
-    const [noveldata, setNovelData] = useState(null);
+    const {novel_id, cpt_no}=useParams();
+    console.log(novel_id);
+    const [chapterdata, setChapterData] = useState([]);
+    const [noveldata, setNovelData] = useState([]);
    useEffect(()=>{
-        const url1="http://127.0.0.1:8000/api/novel-info/chapters?novel_id="+id;
-        const url2="http://127.0.0.1:8000/api/novels/novel/?novel_id="+id;
+        const url1="http://127.0.0.1:8000/api/chapter/contents/?novel_id="+novel_id+"&cpt_no="+cpt_no;
+        const url2="http://127.0.0.1:8000/api/novels/novel/?novel_id="+novel_id;
        Promise.all( 
         [
             axios.get(url1),
@@ -21,19 +22,35 @@ const ChapterLayout=()=>{
             setNovelData(novelDataResponse.data);
         })
         .catch((error)=>{
-            alert(error.message);
+            console.log(novel_id);
+            console.log(error.message);
           })
 
-   },[id]);
+   },[novel_id,cpt_no]);
 
-
+   
     return(
+        
         <div>
             <div class="content-wrap">
             <article id="chapter-article">
             <section className="page-in content-wrap"/>
-                    <CptIntro/>
-                    <CptContent/>
+                    {chapterdata.map((data)=>(
+                        <CptIntro
+                        key={data.descript_id}
+                        chapter_title={data.chapter_title}
+                        novel_id={data.novel_id}
+                        novelName={noveldata[0].novel_name} 
+                         />
+                    ))}
+                    
+                    {chapterdata.map((data)=>(
+                        <CptContent
+                        key={data.descript_id}
+                        cpt_text={data.cpt_text} 
+                         />
+                    ))}
+                
             </article>
             </div>
         </div>

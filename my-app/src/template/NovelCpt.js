8fef9d6abfbabf65832292b7e4a4e7f58b1e92f8
Utchash007 <shariarhasan872@gmail.com>
2024-12-18 17:26:34 +0600

    Update API

--- my-app/src/template/NovelCpt.js
+++ my-app/src/template/NovelCpt.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; 
 import ChapterLayout from "../Layout/ChapterLayout";
-const Novel_cpt=()=>{
+const Novel_cpt=({chapters})=>{
+    
     return(
 <div>
         <div class="chapter-list">
     <h2>Chapters</h2>
+    
     <ul>
-        <li><Link to ="/ChapterLayout">Chapter 1</Link></li>
-        <li><a href="chapterPage.php?novel_id=1&chapter_no=2&chapter_title=Chapter+2&chapter_text=This+is+chapter+2+text">Chapter 2</a></li>
-        <li><a href="chapterPage.php?novel_id=1&chapter_no=3&chapter_title=Chapter+3&chapter_text=This+is+chapter+3+text">Chapter 3</a></li>
+    {chapters.map((chapter) => (
+                        <li key={chapter.descript_id}>
+                            <Link to={`/ChapterLayout/${chapter.novel_id}/${chapter.cpt_no}`}>
+                                Chapter {chapter.cpt_no}: {chapter.chapter_title}
+                            </Link>
+                        </li>
+                    ))}
     </ul>
     </div>
 </div>

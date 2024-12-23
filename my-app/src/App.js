5531ba144d26c5293b361922012d50500f14adc5
Utchash007 <shariarhasan872@gmail.com>
2024-12-23 15:03:24 +0600

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Header from "./Header_Components/heder";
 import Slider from "./template/Slider";
 import Card from "./template/card";
@@ -9,6 +9,12 @@ import Novelpage from "./Layout/Novelpage";
 import ChapterLayout from "./Layout/ChapterLayout";
 import EditProfileLayout from "./Layout/EditProfileLayout";
 import SearchResultLayout from "./Layout/SearchResultLayout";
+import { isAuthenticated } from "./authUtils";
+import LoginSignup from "./LoginSignup/LoginSignup";
+
+const PrivateRoute = ({ element }) => {
+  return isAuthenticated() ? element : <Navigate to="/LoginSignup" replace />;
+};
 
 const App = () => {
   return (
@@ -16,12 +22,14 @@ const App = () => {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<PrivateRoute element={<Homepage />} />} />
           {/* Add the :id parameter here for Novelpage */}
-          <Route path="/Novelpage/:id" element={<Novelpage />} />
-          <Route path="/ChapterLayout/:novel_id/:cpt_no" element={<ChapterLayout />} />
+          <Route path="/Novelpage/:id" element={<PrivateRoute element={<Novelpage />} />} />
+          <Route path="/ChapterLayout/:novel_id/:cpt_no" element={<ChapterLayout />}/>
           <Route path="/EditProfileLayout" element={<EditProfileLayout />} />
           <Route path="/SearchResultLayout/:searchQuery" element={<SearchResultLayout />} />
+
+          <Route path="/LoginSignup" element={<LoginSignup/>} />
         </Routes>
       </Router>
     </div>

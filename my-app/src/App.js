b41a461631013902bbd5d2d63af550946ad4b766
Utchash007 <shariarhasan872@gmail.com>
2024-12-24 11:48:40 +0600

    added authentication

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,49 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header_Components/heder";
-import Slider from "./template/Slider";
-import Card from "./template/card";
 import Homepage from "./Layout/homepage";
-import "./styles.css";
 import Novelpage from "./Layout/Novelpage";
 import ChapterLayout from "./Layout/ChapterLayout";
 import EditProfileLayout from "./Layout/EditProfileLayout";
 import SearchResultLayout from "./Layout/SearchResultLayout";
-import { isAuthenticated } from "./authUtils";
 import LoginSignup from "./LoginSignup/LoginSignup";
+import PrivateRouter from "./PrivateRouter";
+import "./styles.css";
 
-const PrivateRoute = ({ element }) => {
-  return isAuthenticated() ? element : <Navigate to="/LoginSignup" replace />;
-};
+import { getToken } from "./Common";
 
 const App = () => {
-  return (
-    <div>
-      <Router>
-        <Header />
-        <Routes>
-        <Route path="/" element={<PrivateRoute element={<Homepage />} />} />
-          {/* Add the :id parameter here for Novelpage */}
-          <Route path="/Novelpage/:id" element={<PrivateRoute element={<Novelpage />} />} />
-          <Route path="/ChapterLayout/:novel_id/:cpt_no" element={<ChapterLayout />}/>
-          <Route path="/EditProfileLayout" element={<EditProfileLayout />} />
-          <Route path="/SearchResultLayout/:searchQuery" element={<SearchResultLayout />} />
 
-          <Route path="/LoginSignup" element={<LoginSignup/>} />
-        </Routes>
-      </Router>
-    </div>
+  const isAuthenticated = getToken();
+
+  return (
+    <Router>
+      {isAuthenticated && <Header />}
+      <Routes>
+        
+        {/* Public Routes */}
+        <Route path="/LoginSignup" element={<LoginSignup />} />
+        
+        {/* Private Routes */}
+        <Route element={<PrivateRouter />}>
+          <Route path="/" element={<Homepage />} />
+        </Route>
+
+
+        <Route element={<PrivateRouter />}>
+          <Route path="/Novelpage/:id" element={<Novelpage />} />
+          <Route
+            path="/ChapterLayout/:novel_id/:cpt_no"
+            element={<ChapterLayout />}
+          />
+          <Route path="/EditProfileLayout" element={<EditProfileLayout />} />
+          <Route
+            path="/SearchResultLayout/:searchQuery"
+            element={<SearchResultLayout />}
+          />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 

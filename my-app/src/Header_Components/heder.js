b0d213bb2d49ba01dcd9e084c0a4377897afa4a3
Utchash007 <shariarhasan872@gmail.com>
2025-01-07 21:44:56 +0600

    Added Navbar functionality

--- my-app/src/Header_Components/heder.js
+++ my-app/src/Header_Components/heder.js
@@ -1,18 +1,44 @@
-import React, { useState } from "react";
-//import ProfileDropdown from "./ProfileDropdown";
+import React, { useState,useEffect, useRef } from "react";
 import SearchBar from "./SearchBar";
 import ProfileDropdown from "./ProfileDropdown";
+import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
-
+import { decode } from "../decodeJWT";
+import axios from "axios";
 const Header = () => {
+  const [userData,setUserData]=useState([]);
+  const user_id=(decode(localStorage.getItem("active"))).user_id;
+  console.log(user_id);
+  const isFetchedRef=useRef(false)
+  useEffect(()=>{
+    if(isFetchedRef.current)return;
+    isFetchedRef.current=true;
+    const url1="http://127.0.0.1:8000/api/users/get_user/?id="+user_id;
+    
+   Promise.all( 
+    [
+      axios.get(url1)
+    ])
+    .then(([userResponse])=>{
+        setUserData(userResponse.data);
+    })
+    .catch((error)=>{
+        console.log(error.message);
+      })
+
+},[user_id]);
+  
   return (
+    <div>
     <header>
       <Link to="/" className="logo">
         <img src="./Assets/NovelNestLogo.png"></img>
       </Link>
       <SearchBar />
-      <ProfileDropdown/>
+      <ProfileDropdown />
     </header>
+    <Navbar />
+    </div>
   );
 };
 

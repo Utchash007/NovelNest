5a3b60a6b1248605bdd0b222c4dfe9b27463b0ed
Shariar Hasan <101088151+Utchash007@users.noreply.github.com>
2025-01-08 17:39:06 +0600

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import LoginSignup from "./LoginSignup/LoginSignup";
+import { isAuthenticated } from "./authUtils"; // Import the authentication function
 
 const Main = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      const authenticated = await isAuthenticated(); 
+      setIsLoggedIn(authenticated); 
+    };
+
+    checkAuthentication(); 
+  }, []);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true); 
+  };
+
   return isLoggedIn ? (
     <App />
   ) : (
-    <LoginSignup onLoginSuccess={() => setIsLoggedIn(true)} />
+    <LoginSignup onLoginSuccess={handleLoginSuccess} />
   );
 };
 
